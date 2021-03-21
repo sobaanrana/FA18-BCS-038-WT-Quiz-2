d96714fd9a7d567c69da781f765a2f9c1525e706
Sobaan <55842323+sobaanrana@users.noreply.github.com>
2021-03-21 14:02:37 +0500

    Update myjscode.js

--- myjscode.js
+++ myjscode.js
@@ -1,9 +1,43 @@
 $(function () {
-    $("#myForm").submit(handleFormSubmit);
-  });
-  
-  function handleFormSubmit(e) {
-    alert("Form is being submitted");
-  
-    e.preventDefault();
+  $("#myForm").submit(handleFormSubmit);
+});
+
+function handleFormSubmit(e) {
+  var isvalid = true;
+  var name = $("#name").val();
+  var pass = $("#password").val();
+  var cpass = $("#confirm_password").val();
+
+  $("#name").removeClass("error");
+  $("#password").removeClass("error");
+  $("#confirm_password").removeClass("error");
+
+  if(name.length <= 4)
+  {
+      $("#name").addClass("error");
+      alert("Name should be atleast 5 characters long");
+      isvalid = false;
+
+
   }
+  if(pass.length < 8)
+  {
+      $("#password").addClass("error");
+      alert("Password must be atleast 8 character long");
+      isvalid = false;
+  }
+  if( (cpass != pass) || cpass.length < 8)
+  {
+      $("#confirm_password").addClass("error");
+      alert("Password must match and greater than or equal 8");
+      isvalid = false;
+  }
+  if(isvalid == false )
+  {
+      e.preventDefault();
+      return;
+  }
+  else{
+      alert("Form is being submitted");
+  }
+}
